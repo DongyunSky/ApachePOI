baeb61ed1ea8a972911f04dd69a5ecbfcc98f88a
prodev <prodev@126.com>
2019-05-24 09:09:10 +0800

    Word.

--- src/main/java/fun/prodev/learn/apache/poi/word/Word.java
+++ src/main/java/fun/prodev/learn/apache/poi/word/Word.java
@@ -2,13 +2,16 @@ package fun.prodev.learn.apache.poi.word;
 
 import fun.prodev.learn.apache.poi.Constants;
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
 import org.apache.poi.xwpf.usermodel.*;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 
 /**
  * @author prodev
@@ -23,28 +26,72 @@ public class Word {
 
         // 创建
         XWPFDocument xwpfDocument = new XWPFDocument();
+
+        // 创建标题
+        XWPFParagraph titleParagraph = xwpfDocument.createParagraph();
+        titleParagraph.setAlignment(ParagraphAlignment.CENTER); // 设置段落居中
+        XWPFRun titleParagraphRun = titleParagraph.createRun();
+
+        titleParagraphRun.setText("Java PoI");
+        titleParagraphRun.setColor("000000");
+        titleParagraphRun.setFontSize(20);
+
         // 段落
         XWPFParagraph paragraph = xwpfDocument.createParagraph();
         XWPFRun run = paragraph.createRun();
         run.setText("At tutorialspoint.com, we strive hard to provide quality tutorials for self-learning purpose in the domains of Academics, Information Technology, Management and Computer Programming Languages.");
 
+        // 换行
+        XWPFParagraph paragraph1 = xwpfDocument.createParagraph();
+        XWPFRun paragraphRun1 = paragraph1.createRun();
+        paragraphRun1.setText("\r");
+
         // 表格
         XWPFTable table = xwpfDocument.createTable();
-        //create first row
-        XWPFTableRow tableRowOne = table.getRow(0);
-        tableRowOne.getCell(0).setText("col one, row one");
-        tableRowOne.addNewTableCell().setText("col two, row one");
-        tableRowOne.addNewTableCell().setText("col three, row one");
-        //create second row
-        XWPFTableRow tableRowTwo = table.createRow();
-        tableRowTwo.getCell(0).setText("col one, row two");
-        tableRowTwo.getCell(1).setText("col two, row two");
-        tableRowTwo.getCell(2).setText("col three, row two");
-        //create third row
-        XWPFTableRow tableRowThree = table.createRow();
-        tableRowThree.getCell(0).setText("col one, row three");
-        tableRowThree.getCell(1).setText("col two, row three");
-        tableRowThree.getCell(2).setText("col three, row three");
+        // 填充数据 表格生成后自带一行一个单元格
+        XWPFTableRow row = table.getRow(0);
+        row.getCell(0).setText("标题列1");
+        row.addNewTableCell().setText("第2个标题列");
+        row.addNewTableCell().setText("第3列");
+        row.addNewTableCell().setText("第4列");
+        for (int i = 0; i < 10; i++) {
+            row = table.createRow();
+            for (int j = 0; j < 4; j++) {
+                row.getCell(j).setText((i+1) + "行" + j + "列");
+            }
+        }
+        table.getCTTbl().getTblPr().unsetTblBorders(); // 去表格边框
+        // 列宽自动分割
+        CTTblWidth infoTableWidth = table.getCTTbl().addNewTblPr().addNewTblW();
+        infoTableWidth.setType(STTblWidth.DXA);
+        infoTableWidth.setW(BigInteger.valueOf(9072));
+
+        CTSectPr sectPr = xwpfDocument.getDocument().getBody().addNewSectPr();
+        XWPFHeaderFooterPolicy policy = new XWPFHeaderFooterPolicy(xwpfDocument, sectPr);
+        // 添加页眉
+        CTP ctpHeader = CTP.Factory.newInstance();
+        CTR ctrHeader = ctpHeader.addNewR();
+        CTText ctHeader = ctrHeader.addNewT();
+        String headerText = "ctpHeader";
+        ctHeader.setStringValue(headerText);
+        XWPFParagraph headerParagraph = new XWPFParagraph(ctpHeader, xwpfDocument);
+        // 设置为右对齐
+        headerParagraph.setAlignment(ParagraphAlignment.RIGHT);
+        XWPFParagraph[] parsHeader = new XWPFParagraph[1];
+        parsHeader[0] = headerParagraph;
+        policy.createHeader(XWPFHeaderFooterPolicy.DEFAULT, parsHeader);
+
+        // 添加页脚
+        CTP ctpFooter = CTP.Factory.newInstance();
+        CTR ctrFooter = ctpFooter.addNewR();
+        CTText ctFooter = ctrFooter.addNewT();
+        String footerText = "ctpFooter";
+        ctFooter.setStringValue(footerText);
+        XWPFParagraph footerParagraph = new XWPFParagraph(ctpFooter, xwpfDocument);
+        headerParagraph.setAlignment(ParagraphAlignment.CENTER);
+        XWPFParagraph[] parsFooter = new XWPFParagraph[1];
+        parsFooter[0] = footerParagraph;
+        policy.createFooter(XWPFHeaderFooterPolicy.DEFAULT, parsFooter);
 
         // 写出
         File newWord = new File(Constants.TEMP_PATH + "new Word.docx");
